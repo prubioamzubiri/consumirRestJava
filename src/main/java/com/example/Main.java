27624b5951578171494b6c045e6c03fbb826a9f4
Pablo Rubio <prubioam@zubirimanteo.com>
2024-10-09 10:07:27 +0200

    first commit

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import java.net.http.HttpClient;
+
+public class Main {
+    public static void main(String[] args) {
+        
+
+        HttpClient client = HttpClient.newHttpClient();
+    }
+}
